69658325233afc90be71323231137f7d0944bc52
mykelcodex <michealoke39@yahoo.com>
2020-07-23 21:39:53 +0100

    fifth Commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,6 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'icon', type: 'image/png', href: '/images/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', href: '/images/favicon-16x16.png' },
     ]
